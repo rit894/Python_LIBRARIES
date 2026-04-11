13a87231aa4c8aeaa1a1fddc14c6a7c129dfe0a8
rit894 <nagarithwikv@gmail.com>
2026-04-11 06:28:22 +0530

    Copiolet failed to generate the commit message

---
+++ Pandas/CleaningData/CD_1.py
@@ -0,0 +1,15 @@
+import pandas as pd
+df = pd.read_csv("D:\Python_LIBRARIES\Pandas\CSVfiles\CD1.csv")
+# df1 = pd.read_csv("D:\Python_LIBRARIES\Pandas\CSVfiles\workout_data.csv")
+# df1.dropna(inplace=True)
+# print(df.to_string())
+# print(df1.to_string())
+
+new_df=df.fillna({"Calories":130})
+# print(new_df.to_string())
+# print(df.to_string())
+x = df["Calories"].median()
+df1 = df.fillna({"Calories":x})
+
+print(df1.to_string())
+
